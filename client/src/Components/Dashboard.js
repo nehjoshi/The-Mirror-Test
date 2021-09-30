445ef1520e942b839b9c55475ec56d47599fde1b
Neh Joshi <nehjoshi5@gmail.com>
2021-09-30 16:54:52 +0530

    Profile icon + logout

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -11,7 +11,7 @@ import Hope from '../Images/hope.png';
 import Bft from '../Images/bft.png';
 import Perma from '../Images/perma.png';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   wrapper: {
     width: "100%",
     minHeight: '100vh',
@@ -49,21 +49,51 @@ const useStyles = makeStyles(() => ({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    fontSize: '1.2rem',
+    fontSize: '1.3rem',
     color: '#fff',
     fontWeight: '900',
+    border: '3px solid transparent',
+    transition: '0.2s all ease',
+    zIndex: '100',
     '&:hover': {
-      cursor: 'pointer',  
+      cursor: 'pointer',
+      borderColor: 'gray',
+    },
+    [theme.breakpoints.down("md")]: {
+      display: 'none'
     }
-  }
+  },
+  profileWrapper: {
+    position: 'absolute',
+    // height: 'px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    backgroundColor: '#003840',
+    right: '20px',
+    fontSize: '1.3rem',
+    padding: '20px 20px',
+    zIndex: '100',
+    borderRadius: '10px',
+    top: 70,
+    color: '#fff'
+  },
+  logOut: {
+    '&:hover': {
+      cursor: 'pointer',
+      textDecoration: 'underline'
+    }
+  },
 
 }))
+
 const Dashboard = () => {
   const theme = useTheme();
   const classes = useStyles(theme);
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
   const history = useHistory();
+  const [profileOpen, setProfileOpen] = useState(false);
 
   useEffect(() => {
     axios
@@ -76,7 +106,7 @@ const Dashboard = () => {
         if (res.data.auth === true) {
           axios.get(`https://self-growth-questionaire.herokuapp.com/fetch/${localStorage.getItem("email")}`)
             .then(res => {
-              console.log(res.data);
+              
               setData(res.data);
               setLoading(false);
             })
@@ -88,29 +118,49 @@ const Dashboard = () => {
         console.log(e);
       });
   }, [history])
+
+  const handleProfileSection = (type) => {
+    if (type==='toggle') setProfileOpen(!profileOpen);
+    else if (type==='hide') setProfileOpen(false);
+  }
+  const handleLogOut = () => {
+    localStorage.clear();
+    sessionStorage.clear();
+    history.push('/');
+  }
+
   return loading ? <Grid className={classes.wrapper}><Loader /></Grid> : (
-    <Grid className={classes.wrapper}>
-      <h1 className={classes.heading}>The Mirror Test</h1>
-      <div className={classes.profile}>{localStorage.getItem("name").charAt(0)}</div>
-      <Grid container className={classes.testContainer}>
-        <TestInfo heading="Adverse Childhood Experience (ACE) Test" imgSource={Ace} height='200px'
-          desc="Adverse Childhood Experiences are what govern our 
+    <>
+      {profileOpen &&
+        <div className={classes.profileWrapper}>
+          <h6>{localStorage.getItem("name")}</h6>
+          <h6>{localStorage.getItem("email")}</h6>
+          <h6 className={classes.logOut} onClick={handleLogOut}>Log Out</h6>
+        </div>
+      }
+      <div className={classes.profile} onClick={() => handleProfileSection('toggle')}>{localStorage.getItem("name").charAt(0)}</div>
+      <Grid className={classes.wrapper} onClick={() => handleProfileSection('hide')}>
+        <h1 className={classes.heading}>The Mirror Test</h1>
+        <Grid container className={classes.testContainer}>
+          <TestInfo heading="Adverse Childhood Experience (ACE) Test" imgSource={Ace} height='200px'
+            desc="Adverse Childhood Experiences are what govern our 
           lives and shape our future. Take this test to find out more" progress={data.quiz1.percentage} testNo={1} lastQ={data.quiz1.lastQ} />
-        <TestInfo heading="Attachment Style Test" imgSource={Att_Style} height="180px"
-          desc="How do you attach with your loved ones or your partner?
+          <TestInfo heading="Attachment Style Test" imgSource={Att_Style} height="180px"
+            desc="How do you attach with your loved ones or your partner?
            Out of the four attachment styles, secure, 
            anxious, avoidant and disoranized, which one are you?" progress={data.quiz2.percentage} testNo={2} lastQ={data.quiz2.lastQ} />
-        <TestInfo heading="Hope and Optimism Test" imgSource={Hope} height="220px"
-          desc="How optimistic or pessimistic are you? And how does your level of hope affect your life? Take this test to gain further insights." progress={data.quiz3.percentage} testNo={3} lastQ={data.quiz3.lastQ} />
-      </Grid>
-      <Grid container className={classes.testContainer}>
-        <TestInfo heading="Big Five Personality Test" imgSource={Bft} height='200px'
-          desc="Out of the five big personality traits, which one suits you the best? Answer these 50 questions to find out more." progress={data.quiz4.percentage} testNo={4} lastQ={data.quiz4.lastQ} />
-        <TestInfo heading="Wellbeing (PERMA) Test" imgSource={Perma} height='200px'
-          desc="How do you feel about your overall wellbeing, quality of life and your general health?
+          <TestInfo heading="Hope and Optimism Test" imgSource={Hope} height="220px"
+            desc="How optimistic or pessimistic are you? And how does your level of hope affect your life? Take this test to gain further insights." progress={data.quiz3.percentage} testNo={3} lastQ={data.quiz3.lastQ} />
+        </Grid>
+        <Grid container className={classes.testContainer}>
+          <TestInfo heading="Big Five Personality Test" imgSource={Bft} height='200px'
+            desc="Out of the five big personality traits, which one suits you the best? Answer these 50 questions to find out more." progress={data.quiz4.percentage} testNo={4} lastQ={data.quiz4.lastQ} />
+          <TestInfo heading="Wellbeing (PERMA) Test" imgSource={Perma} height='200px'
+            desc="How do you feel about your overall wellbeing, quality of life and your general health?
           Take the wellbeing test to know more about this." progress={data.quiz5.percentage} testNo={5} lastQ={data.quiz5.lastQ} />
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   )
 }
 export default Dashboard;
