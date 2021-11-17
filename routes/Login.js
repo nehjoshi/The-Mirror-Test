6195aa6a2ff7407622ead1d53b197380c5965a40
Neh Joshi <nehjoshi5@gmail.com>
2021-11-17 12:08:00 +0530

    Cors

--- routes/Login.js
+++ routes/Login.js
@@ -11,6 +11,15 @@ const limiter = rateLimit({
     max: 100
 })
 Login.use(limiter);
+Login.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  
+    next();
+  });
 
 Login.post('/login', async (req, res) => {
     const User = await mongoose.model('The Mirror Test', schema);
