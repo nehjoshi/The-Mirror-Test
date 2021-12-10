021ee52bb79cd8b58ef351386ded0fb8cd04cd2f
Neh Joshi <nehjoshi5@gmail.com>
2021-12-10 22:24:37 +0530

    Result progress

--- client/src/Components/Results.js
+++ client/src/Components/Results.js
@@ -5,7 +5,7 @@ import { useHistory } from "react-router-dom";
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import ResultPDF from "./ResultPDF.js";
 import ResultPDFWithoutDetails from "./ResultPDFWithoutDetails";
-import {Table, TableCell, TableContainer, TableHead, TableRow, Paper} from '@material-ui/core';
+import { Table, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
 import { useStyles } from "./Styles/ResultStyles";
 
 const Results = () => {
@@ -13,6 +13,7 @@ const Results = () => {
   const classes = useStyles(theme);
   const [loading, setLoading] = useState(true);
   const [skipped, setSkipped] = useState(false);
+  const [result, setResult] = useState([]);
   const history = useHistory();
   const loadingRef1 = useRef(null);
 
@@ -28,6 +29,8 @@ const Results = () => {
         if (res.data.auth === true) {
           setTimeout(() => {
             setSkipped(res.data.completeData.skippedDetails);
+            setResult(res.data.completeData.result);
+            console.log(res.data.completeData);
             setLoading(false);
           }, 100)
         } else {
@@ -40,6 +43,13 @@ const Results = () => {
 
   }, [history])
 
+  const green = {
+    backgroundColor: '#00cc30',
+  }
+  const red = {
+    backgroundColor: '#FFBF00',
+  }
+
   return loading ? (
     <Grid container className={classes.wrapper}>
       <div ref={loadingRef1}>Loading...</div>
@@ -51,29 +61,55 @@ const Results = () => {
           <h1 className={classes.logo}>The Mirror Test</h1>
           <h1 className={classes.heading}>Results</h1>
         </Grid>
-        <Grid className={classes.section1}>
+        <Grid className={classes.section}>
           <Grid className={classes.section1Header}>
-            <h1 className={classes.section1Heading}>Basic Information</h1>
+            <h1 className={classes.heading}>Basic Information</h1>
           </Grid>
-          <TableContainer component={Paper} style={{width: '80%', margin: '20px auto'}}>
+          <TableContainer component={Paper} style={{ width: '80%', margin: '20px auto' }}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
               <TableHead>
                 <TableRow>
-                  <TableCell><strong style={{fontSize: '1.5rem', color: "#005751"}}>Name</strong></TableCell>
-                  <TableCell align="right"><strong style={{fontSize: '1.5rem'}}>Neh Samir Joshi</strong></TableCell>
+                  <TableCell><strong style={{ fontSize: '1.5rem', color: "#005751" }}>Name</strong></TableCell>
+                  <TableCell align="right"><strong style={{ fontSize: '1.5rem' }}>Neh Samir Joshi</strong></TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell><strong style={{fontSize: '1.5rem', color: "#005751"}}>Email</strong></TableCell>
-                  <TableCell align="right"><strong style={{fontSize: '1.5rem'}}>nehjoshi5@gmail.com</strong></TableCell>
+                  <TableCell><strong style={{ fontSize: '1.5rem', color: "#005751" }}>Email</strong></TableCell>
+                  <TableCell align="right"><strong style={{ fontSize: '1.5rem' }}>nehjoshi5@gmail.com</strong></TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell><strong style={{fontSize: '1.5rem', color: "#005751"}}>Nationality</strong></TableCell>
-                  <TableCell align="right"><strong style={{fontSize: '1.5rem'}}>British</strong></TableCell>
+                  <TableCell><strong style={{ fontSize: '1.5rem', color: "#005751" }}>Nationality</strong></TableCell>
+                  <TableCell align="right"><strong style={{ fontSize: '1.5rem' }}>British</strong></TableCell>
                 </TableRow>
               </TableHead>
             </Table>
           </TableContainer>
         </Grid>
+        <Grid className={classes.section}>
+          <h1 className={classes.heading}>Your Results</h1>
+          <h2 className={classes.subHeading}>Part 1 - Adverse Childhood Experience (ACE)</h2>
+          <Grid className={classes.resultBox}>
+            <Grid className={classes.resultBoxHeader}>
+              <h1 style={{ fontSize: '3rem' }}>Your ACE score is: </h1>
+              <div className={classes.numCircle} style={result.quiz1.result >= 4 ? red : green}>
+                <h1 style={{ fontSize: '3rem', color: "#fff" }}>{result.quiz1.result}</h1>
+              </div>
+            </Grid>
+            <p style={{ fontSize: '1.2rem' }}>An ACE score is a tally of different types of abuse, neglect, and other hallmarks of a rough childhood. According to the Adverse Childhood Experiences study, the rougher your childhood, the higher your score is likely to be and the higher your risk for later health problems. </p>
+          </Grid>
+
+          <h2 className={classes.subHeading}>Part 2 - Attachment Style</h2>
+          <Grid className={classes.resultBox}>
+            <Grid className={classes.resultBoxHeader}>
+              <h1 style={{ fontSize: '3rem' }}>Your attachment style is: </h1>
+                <h1 style={{ fontSize: '3rem' }}>{result.quiz2.result}</h1>
+            </Grid>
+            <p style={{ fontSize: '1.2rem' }}>An ACE score is a tally of different types of abuse, neglect, and other hallmarks of a rough childhood. According to the Adverse Childhood Experiences study, the rougher your childhood, the higher your score is likely to be and the higher your risk for later health problems. </p>
+          </Grid>
+
+        </Grid>
+
+
+
         {skipped === false ?
           <PDFDownloadLink document={<ResultPDF />} fileName="Results.pdf">
             {({ blob, url, loading, error }) => (loading ? "Loading..." : <div className={classes.button} >Download Results</div>)}
--- client/src/Components/Styles/ResultStyles.js
+++ client/src/Components/Styles/ResultStyles.js
@@ -4,18 +4,15 @@ import Bg from '../../Images/bg.jpg';
 export const useStyles = makeStyles((theme) => ({
     wrapper: {
         width: "100%",
-        height: "100vh",
         backgroundImage: `url(${Bg})`,
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center center",
         display: "flex",
         justifyContent: "center",
-        alignItems: "center",
-        overflow: "hidden",
     },
     box: {
-        height: "650px",
+        marginTop: "30px",
         width: "95%",
         boxShadow: "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
         backgroundColor: "#f7f7f7",
@@ -48,10 +45,35 @@ export const useStyles = makeStyles((theme) => ({
     heading: {
         fontSize: "2.2rem",
     },
-    section1: {
+    subHeading: {
+        fontSize: "1.6rem",
+        color: "#005751",
+        textDecoration: "underline",
+        marginTop: "10px",
+    },
+    section: {
         width: '80%',
         display: "flex",
         flexDirection: "column",
         margin: "30px auto"
+    },
+    resultBox: {
+        width: "60%",
+        padding: "10px 10px",
+        boxShadow: "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
+        margin: "20px auto",
+    },
+    resultBoxHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+    },
+    numCircle: {
+        width: "100px",
+        height: "100px",
+        borderRadius: '50%', 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center'
     }
 }))
